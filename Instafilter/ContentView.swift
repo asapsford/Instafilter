291d96dcc0e5d61d15a7c2395ec96328802a27fa
Alex Sapsford <asapsford11@gmail.com>
2020-09-29 11:48:47 +0100

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Alex Sapsford on 29/09/2020.
+//  Copyright © 2020 Alex Sapsford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
